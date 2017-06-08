e73b485f69be2003403feca3338a9dac902890f6
Waszka <filip.toka@wp.pl>
2017-06-08 16:45:28 +0200

    dupaczek

---
+++ app/src/main/java/waszka/kalkocznik/FiguresActivity.java
@@ -0,0 +1,13 @@
+package waszka.kalkocznik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FiguresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_figures);
+    }
+}
