8fa5e290808116aa48f4c2568c08776ecdbbbdb6
Waszka <filip.toka@wp.pl>
2017-06-08 22:20:53 +0200

    Convert

--- app/src/main/java/waszka/kalkocznik/MenuActivity.java
+++ app/src/main/java/waszka/kalkocznik/MenuActivity.java
@@ -1,7 +1,9 @@
 package waszka.kalkocznik;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,4 +12,35 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
+
+    public void GoSys(View view)
+    {
+        Intent myIntent = new Intent(MenuActivity.this, SystemyActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void GoCal(View view)
+    {
+        Intent myIntent = new Intent(MenuActivity.this, CalculatorActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void GoFig(View view)
+    {
+        Intent myIntent = new Intent(MenuActivity.this, FiguresActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void GoExit(View view)
+    {
+        finish();
+    }
+
+
+
+
+
+
+
+
 }
